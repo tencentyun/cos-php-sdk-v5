5773404ba0b69ff512eeea87d67f26aec4f7f46f
lewzylu <327874225@qq.com>
2017-09-13 16:44:19 +0800

    Update Client.php

--- src/Qcloud/Cos/Client.php
+++ src/Qcloud/Cos/Client.php
@@ -11,7 +11,7 @@ use Qcloud\Cos\Signature;
 use Qcloud\Cos\TokenListener;
 
 class Client extends GSClient {
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     private $region;       // string: region.
     private $credentials;
