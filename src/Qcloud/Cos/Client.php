13ba31a7b6cf6722428305c1d713718b5ca70c0f
lewzylu <327874225@qq.com>
2017-09-26 16:55:16 +0800

    Update to 1.1.0

--- src/Qcloud/Cos/Client.php
+++ src/Qcloud/Cos/Client.php
@@ -11,7 +11,7 @@ use Qcloud\Cos\Signature;
 use Qcloud\Cos\TokenListener;
 
 class Client extends GSClient {
-    const VERSION = '1.0.1';
+    const VERSION = '1.1.0';
 
     private $region;       // string: region.
     private $credentials;
