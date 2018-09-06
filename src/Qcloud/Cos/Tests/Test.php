285a954d0a8d258d97090e31c1f6e160291194f7
lewzylu <327874225@qq.com>
2018-09-06 21:54:55 +0800

    Update Test.php

--- src/Qcloud/Cos/Tests/Test.php
+++ src/Qcloud/Cos/Tests/Test.php
@@ -17,7 +17,7 @@ class BucketTest extends \PHPUnit_Framework_TestCase
                 'appId' => getenv('COS_APPID'),
                 'secretId' => getenv('COS_KEY'),
                 'secretKey' => getenv('COS_SECRET'))));
-        sleep(5);
+        sleep(8);
     }
 
     protected function tearDown()
