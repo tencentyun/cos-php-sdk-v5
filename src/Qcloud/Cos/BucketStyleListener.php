b90877ac25b042a3d8f2863bf6f952e611170226
lewzylu <327874225@qq.com>
2019-01-28 16:21:13 +0800

    feature-ip-port

--- src/Qcloud/Cos/BucketStyleListener.php
+++ src/Qcloud/Cos/BucketStyleListener.php
@@ -73,6 +73,7 @@ class BucketStyleListener implements EventSubscriberInterface {
         {
             $bucket = $bucket.'-'.$this->appId;
         }
+        $request->setPath(urldecode($request->getPath()));
         $request->getParams()->set('bucket', $bucket)->set('key', $key);
 
         $realHost = $bucket. '.' . $request->getHost();
