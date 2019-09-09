f0903545ab3cd2e7f79d37dc17427d0c95985f4e
安正超 <anzhengchao@gmail.com>
2019-09-09 16:55:56 +0800

    Fixed typo.

--- src/Qcloud/Cos/UploadBodyMiddleware.php
+++ src/Qcloud/Cos/UploadBodyMiddleware.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use GuzzleHttp\Exception\RequestException;
 
-class SignatureMiddleware {
+class UploadBodyMiddleware {
     private $nextHandler;
     protected $signature;
 
