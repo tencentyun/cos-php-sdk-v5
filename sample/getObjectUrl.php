e11f2767eda9152f1c2ce40933f04ffe2160ed51
沈唁 <52o@qq52o.cn>
2021-04-08 10:28:50 +0800

    Fix syntax errors (#181)
    close https://github.com/tencentyun/cos-php-sdk-v5/issues/180

--- sample/getObjectUrl.php
+++ sample/getObjectUrl.php
@@ -9,22 +9,29 @@ $cosClient = new Qcloud\Cos\Client(
     array(
         'region' => $region,
         'schema' => 'https', //协议头部，默认为http
-        'credentials'=> array(
-            'secretId'  => $secretId ,
-            'secretKey' => $secretKey)));
+        'credentials' => array(
+            'secretId' => $secretId,
+            'secretKey' => $secretKey
+        )
+    )
+);
 $local_path = "/data/exampleobject";
 
-try {    
+try {
     $bucket = "examplebucket-1250000000"; //存储桶，格式：BucketName-APPID
     $key = "exampleobject"; //对象在存储桶中的位置，即对象键
-    $signedUrl = $cosClient->getObjectUrl($bucket,
-                                          $key, 
-                                          '+10 minutes' //签名的有效时间
-                                          ['ResponseContentDisposition' => '111',
-                                           'Params' => [ // Params中可以传自定义querystring
-                                               'aaa' => 'bbb',
-                                               'ccc' => 'ddd'
-                                           ]]); 
+    $signedUrl = $cosClient->getObjectUrl(
+        $bucket,
+        $key,
+        '+10 minutes', //签名的有效时间
+        [
+            'ResponseContentDisposition' => '111',
+            'Params' => [ // Params中可以传自定义querystring
+                'aaa' => 'bbb',
+                'ccc' => 'ddd'
+            ]
+        ]
+    );
     // 请求成功
     echo $signedUrl;
 } catch (\Exception $e) {
